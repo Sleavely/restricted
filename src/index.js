98ba4d4ac876ccc8da7e5e97857fdc39846cba8e
Joakim Hedlund <contact@joakimhedlund.com>
2018-12-17 20:34:26 +0100

    Export aclGroup and permissionScopeManager classes for the lulz

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 'use strict'
 const aclManager = require('./aclManager')
 const aclGroup = require('./aclGroup')
+const permissionScopeManager = require('./permissionScopeManager')
 
 module.exports = {
-  aclManager
+  aclManager,
+  aclGroup,
+  permissionScopeManager,
 }
