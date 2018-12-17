5a3332278cbbf04f7088642a3e62288004430e3d
Joakim Hedlund <joakim.hedlund@danielwellington.com>
2018-12-17 17:40:29 +0100

    Basic permission scope manager

---
+++ src/permissionScopeManager.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const _ = {
+  get: require('lodash.get'),
+  set: require('lodash.set'),
+  unset: require('lodash.unset'),
+}
+
+class PermissionScopeManager {
+
+  constructor()
+  {
+    this.permissionMap = {}
+  }
+
+  default(allowed)
+  {
+    this.permissionMap['$'] = allowed
+  }
+
+  evaluate(prop)
+  {
+    // Start with foo.bar.baz, then try foo.bar, etc.
+    const scopeComponents = prop.split('.')
+    for(let i = scopeComponents.length; i > 0; i--)
+    {
+      const scope = scopeComponents.slice(0,i).join('.')
+      if (typeof this.get(scope) !== 'undefined') return this.get(scope)
+    }
+    // Default
+    return this.permissionMap['$']
+  }
+
+  get(scope)
+  {
+    return _.get(this.permissionMap, scope + '.$')
+  }
+  set(scope, val)
+  {
+    return _.set(this.permissionMap, scope + '.$', !!val)
+  }
+  remove(scope) {
+    return _.unset(this.permissionMap, scope + '.$')
+  }
+
+  // List all scopes that have an explicit setting
+  list(foundPaths, currentPath)
+  {
+    foundPaths = foundPaths || []
+
+    const targetObj = (!currentPath ? this.permissionMap : _.get(this.permissionMap, currentPath))
+    for(let nextPath in targetObj)
+    {
+      if (nextPath === '$' && typeof targetObj[nextPath] !== 'undefined') foundPaths.push(currentPath)
+      if (nextPath !== '$') foundPaths = this.list(foundPaths, (currentPath ? currentPath + '.' : '') + nextPath)
+    }
+    return foundPaths
+  }
+
+  toJSON() {
+    return this.list()
+  }
+}
+
+module.exports = PermissionScopeManager
---
+++ test/permissionScopeManager.spec.js
@@ -0,0 +1,84 @@
+const { expect } = require('chai')
+
+const PermissionScopeManager = require('./../src/permissionScopeManager.js')
+
+describe('PermissionScopeManager', function(){
+  beforeEach(function(){
+    this.scopes = new PermissionScopeManager
+  })
+
+  describe('get()', function () {
+    it('can get an exact set scope')
+  })
+
+  describe('set()', function () {
+    it('can set a scope')
+
+    it('creates nested tree when setting a multi-tiered scope')
+  })
+
+  describe('remove()', function () {
+    it('does not break when removing a non-existent scope', function() {
+      this.scopes.remove('foo.bar')
+      expect(this.scopes.permissionMap).to.not.have.property('foo')
+    })
+
+    it('removing a parent does not remove explicitly set children', function() {
+      this.scopes.set('page', true)
+      this.scopes.set('page.login', true)
+      expect(this.scopes.permissionMap.page).to.have.property('$')
+      this.scopes.remove('page')
+      expect(this.scopes.permissionMap).to.have.property('page')
+      expect(this.scopes.permissionMap.page).to.not.have.property('$')
+      expect(this.scopes.permissionMap.page).to.have.property('login')
+
+    })
+  })
+  
+  describe('evaluate()', function () {
+
+    it('returns the value of a scope', function() {
+      this.scopes.set('foo.bar.baz', true)
+      expect(this.scopes.evaluate('foo.bar.baz')).to.be.true
+    })
+
+    it('traverses upwards until it finds an existing scope', function() {
+      this.scopes.set('blog', true)
+      expect(this.scopes.evaluate('blog.comment.post')).to.be.true
+    })
+
+    it('can have explicitly disallowed paths in a generally allowed tree', function() {
+      this.scopes.set('user', true)
+      this.scopes.set('user.edit.password', false)
+      expect(this.scopes.evaluate('user')).to.be.true
+      expect(this.scopes.evaluate('user.edit')).to.be.true
+      expect(this.scopes.evaluate('user.edit.password')).to.be.false
+      expect(this.scopes.evaluate('user.edit.password.plaintext')).to.be.false
+      expect(this.scopes.evaluate('user.edit.photo')).to.be.true
+    })
+  })
+
+  describe('default()', function () {
+    it('lets you set a default value', function () {
+      expect(this.scopes.evaluate('foo.bar.doesnt.exist')).to.be.undefined
+      this.scopes.default(true)
+      expect(this.scopes.evaluate('foo.bar.doesnt.exist')).to.be.true
+    })
+  })
+  
+  describe('list()', function () {
+    it('returns explicitly defined scopes', function () {
+      this.scopes.set('user.edit.name', true)
+      this.scopes.set('user.edit', false)
+      expect(this.scopes.list()).to.deep.equal(['user.edit.name', 'user.edit'])
+    })
+  })
+  
+  describe('toJSON()', function () {
+    it('serializes a list of defined scopes when stringified', function() {
+      this.scopes.set('user.edit', true)
+      expect(JSON.stringify(this.scopes)).to.equal('["user.edit"]')
+    })
+  })
+  
+})
